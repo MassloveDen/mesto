3729ccb9b96493dcb5697f9f6a713533d652db83
MassloveDen <78202134+MassloveDen@users.noreply.github.com>
2023-04-17 16:33:19 +0300

    fix popup

--- src/scripts/PopupWithForm.js
+++ src/scripts/PopupWithForm.js
@@ -1,36 +1,40 @@
 import { Popup } from "./Popup.js";
 
 export class PopupWithForm extends Popup {
-    constructor(popupSelector, handleFormSubmit) {
-        super(popupSelector);
-        this._handleFormSubmit = handleFormSubmit;
-        // #TODO
-        this._formElement = this._popupElement.querySelector('.popup__form_type_add-card')
-        this.inputs = Array.from(this._formElement.querySelectorAll('.popup__input'))
-    }
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);
+    this._handleFormSubmit = handleFormSubmit;
+    // #TODO
+    this._formElement = this._popupElement.querySelector(
+      ".popup__form_type_add-card"
+    );
+    this.inputs = Array.from(
+      this._formElement.querySelectorAll(".popup__input")
+    );
+  }
 
-    _getInputValue() {
-        const values = {}
+  _getInputValue() {
+    const values = {};
 
-        this._inputs.forEach(input => {
-            const name = input.name
-            const value = input.value
+    this._inputs.forEach((input) => {
+      const name = input.name;
+      const value = input.value;
 
-            values[name] = value
-        });
-        return values
-    }
+      values[name] = value;
+    });
+    return values;
+  }
 
-    setEventListeners() {
-        super.setEventListeners()
-        this._formElement.addEventListener('submit', (e) => {
-            this._handleFormSubmit(e, this._getInputValues())
-            this.close();
-        })
-    }
+  setEventListeners() {
+    super.setEventListeners();
+    this._formElement.addEventListener("submit", (e) => {
+      this._handleFormSubmit(e, this._getInputValues());
+      this.close();
+    });
+  }
 
-    close() {
-        super.close()
-        this._formElement.reset()
-    }
+  close() {
+    super.close();
+    this._formElement.reset();
+  }
 }
